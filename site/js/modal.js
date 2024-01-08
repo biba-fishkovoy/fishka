842830fe0623f5a07a2c5f8183640debd46c2f31
Vivcharuk Rostislav <sunz89@yandex.ru>
2024-01-08 12:33:49 +0300

    Improve laureats page:  - Add single global close button for all modal windows;  - Make background full dimmed & blurred (including header and navbar)    while modal window is open.

--- site/js/modal.js
+++ site/js/modal.js
@@ -1,21 +1,34 @@
 let isModalOpen = false;
 
-function toggleModal(id) {
-  console.log(id);
-  let modal = document.getElementById(id);
-  let mainContainer = document.getElementById('container');
-
-  isModalOpen = modal.classList.toggle('open');
-  mainContainer.classList.toggle('background');
-
-  // document.getElementsByTagName('main').toggle('body-overlay');
-  document.body.classList.toggle('body-overlay');
-}
-
 function previewClickHandler(id) {
   if (isModalOpen) {
     return;
   }
 
   toggleModal(id);
+}
+
+function toggleModal(id=null) {
+  if (id == null) {
+    const modals = Array.from(document.getElementsByClassName("modal open"));
+    modals.forEach(modal => {
+      isModalOpen = modal.classList.toggle('open');
+    });
+  } else { 
+    const modal = document.getElementById(id);
+    isModalOpen = modal.classList.toggle('open');
+  }
+
+  const mainContainer = document.getElementById('container');
+  const overlay = document.getElementById('overlay');
+  const closeButton = document.getElementById('btn-modal-close');
+  const header = document.getElementsByTagName('header')[0];
+  const navbar = document.getElementsByTagName('nav')[0];
+
+  document.body.style.overflow = (document.body.style.overflow == 'hidden') ? '' : 'hidden';
+  overlay.style.display = (overlay.style.display == 'none') ? '' : 'none';
+  mainContainer.classList.toggle('background');
+  closeButton.style.visibility = (isModalOpen) ? 'visible' : 'hidden';
+  header.classList.toggle('background');
+  navbar.classList.toggle('background'); 
 }
